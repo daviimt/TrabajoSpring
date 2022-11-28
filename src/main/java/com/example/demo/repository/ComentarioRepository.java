b812809cebb552eff8c56c8f8d1b6f85339401af
MartaRondan96 <martarondan96@gmail.com>
2022-11-28 18:56:31 +0100

    Repositorios

---
+++ src/main/java/com/example/demo/repository/ComentarioRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.repository;
+
+import java.io.Serializable;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.demo.entity.Comentario;
+
+@Repository("comentarioRepository")
+public interface ComentarioRepository extends JpaRepository<Comentario, Serializable>{
+
+}
