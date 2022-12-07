35fca70de02edaf3798e5368c9bd5efeb3411532
MartaRondan96 <martarondan96@gmail.com>
2022-12-07 18:10:20 +0100

    list Usuarios

--- src/main/java/com/example/demo/controller/UsuarioController.java
+++ src/main/java/com/example/demo/controller/UsuarioController.java
@@ -3,9 +3,11 @@ package com.example.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.demo.serviceImpl.UsuarioService;
@@ -13,11 +15,19 @@ import com.example.demo.serviceImpl.UsuarioService;
 @Controller
 @RequestMapping("/usuarios")
 public class UsuarioController {
-	
+	private static final String USUARIOS_VIEW ="Usuarios";
 	//Inyectamos el servicio de profesor
 	@Autowired
 	@Qualifier("usuarioService")
 	private UsuarioService usuarioService;
+	
+	//Mostrar profesores
+	@GetMapping("/listUsuarios")
+	public ModelAndView listUsuarios() {
+		ModelAndView mav = new ModelAndView(USUARIOS_VIEW);
+		mav.addObject("usuarios", usuarioService.listAllUsuarios());
+		return mav;
+	}
 	
 	//Metodo de activar/descativar 
 	@PostMapping("/activarUsuario/{username}")
