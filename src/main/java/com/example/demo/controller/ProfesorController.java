359af3a74993406b50218dff61efe3c203748195
David <dmateomerino@gmail.com>
2022-12-09 13:57:41 +0100

    uso de authority

--- src/main/java/com/example/demo/controller/ProfesorController.java
+++ src/main/java/com/example/demo/controller/ProfesorController.java
@@ -2,9 +2,9 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -17,9 +17,10 @@ public class SecurityConfig {
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.authorizeRequests((requests) -> requests
 				.antMatchers("/", "/home", "/auth/**", "/about/**", "/noticias", "/error/**", "/webjars/**", "/css/**", "/files/**", "/imgs/**", "/photos/**").permitAll()
-				.antMatchers("/cursos/**", "/matricula/**").access("hasRole('ROL_ALUMNO') or hasRole('ROL_ADMIN')")
-				.antMatchers("/alumnos/**", "profesor/**").access("hasRole('ROL_PROFESOR') or hasRole('ROL_ADMIN')")
-				.antMatchers("/adminPage/**", "/noticias/**").access("hasRole('ROL_ADMIN')")
+				.antMatchers("/cursos/**", "/matricula/**").hasAuthority("ROL_ADMIN or ROL_ALUMNO")
+				.antMatchers("/profesores/**").hasAuthority("ROL_ADMIN or ROL_PROFESOR")
+				.antMatchers("/alumnos/**").hasAuthority("ROL_ADMIN or ROL_ALUMNO")
+				.antMatchers("/adminPage/**", "/noticias/**").hasAuthority("ROL_ADMIN")
 				.anyRequest().authenticated()
 			)
 			.formLogin((form) -> form
