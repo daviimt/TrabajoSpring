1ad63809d4cefc6794edf62c4fa913a0aed65004
David <dmateomerino@gmail.com>
2023-01-27 20:31:40 +0100

    securityConfig

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -16,29 +16,44 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.authorizeRequests((requests) -> requests
-				.antMatchers("/", "/home", "/auth/**", "/about/**", "/noticias/**", "/error/**", "/webjars/**", "/css/**", "/files/**", "/images/**", "/photos/**").permitAll()
-				.antMatchers("/cursos/**", "/matricula/**").hasAnyAuthority("ROL_ADMIN", "ROL_PROFESOR","ROL_ALUMNO")
-				.antMatchers("/profesores/addProfesor","/profesores/formProfesor/**").hasAnyAuthority("ROL_ADMIN", "ROL_PROFESOR")
-				.antMatchers("/profesores/listProfesores","/profesores/deleteProfesor/**","/profesores/formProfesor/").hasAuthority("ROL_ADMIN")
-				.antMatchers("/alumnos/addAlumno","/alumnos/formAlumno/**").hasAnyAuthority("ROL_ADMIN", "ROL_ALUMNO")
-				.antMatchers("/alumnos/listAlumnos","/alumnos/deleteAlumno/**","/alumnos/formAlumno/").hasAuthority("ROL_ADMIN")
-				.antMatchers("/adminPage/**").hasAuthority("ROL_ADMIN")
-				.anyRequest().authenticated()
-			)
-			.formLogin((form) -> form
-				.loginPage("/auth/login")
-				.defaultSuccessUrl("/home")
+				.antMatchers("/", "/home", "/auth/**", "/about/**", "/error/**", "/webjars/**",
+						"/css/**", "/files/**", "/images/**", "/photos/**")
 				.permitAll()
-			)
-			.logout((logout) -> logout.permitAll().
-					logoutUrl("/auth/logout").
-					logoutSuccessUrl("/auth/login?logout"));
+				.antMatchers("/alumnos/formAlumno/**", "/cursos/listCursosAlumno", "/cursos/filtroCursosBasicos",
+						"/cursos/filtroCursosMedios", "/cursos/filtroCursosAvanzados", "/comentario/formComentario/**")
+				.hasAuthority("ROL_ALUMNO")
+				.antMatchers("/profesores/formProfesor/**", "/cursos/filtroCursosFechas/**",
+						"/cursos/filtroCursosAcabados", "/cursos/filtroCursosSinEmpezar",
+						"/cursos/filtroCursosImpartiendose", "/cursos/formCursoProfesor/**",
+						"/cursos/deleteCursoProfesor/**")
+				.hasAuthority("ROL_PROFESOR")
+				.antMatchers("/profesores/listProfesores", "/profesores/formProfesor/**",
+						"/profesores/deleteProfesor/**", "/cursos/listCursos", "/cursos/cursosRanking",
+						"/cursos/formCurso/**", "/cursos/deleteCurso/**", "/alumnos/listAlumnos",
+						"/alumnos/deleteAlumno/**", "/alumnos/activarUsuario/**", "/alumnos/ordenarAlumnos",
+						"/noticias/listNoticias", "/noticias/formNoticia/**", "/noticias/deleteNoticia/**")
+				.hasAuthority("ROL_ADMIN")
+				.antMatchers("/cursos/inscritos/**", "/cursos/ordenarNotas/**")
+				.hasAnyAuthority("ROL_PROFESOR", "ROL_ADMIN").antMatchers("/alumnos/**", "/matricula/**")
+				.hasAnyAuthority("ROL_ADMIN", "ROL_PROFESOR", "ROL_ALUMNO")
+				.antMatchers("/profesores/addProfesor", "/profesores/formProfesor/**")
+				.hasAnyAuthority("ROL_ADMIN", "ROL_PROFESOR")
+				.antMatchers("/profesores/listProfesores", "/profesores/deleteProfesor/**", "/profesores/formProfesor/")
+				.hasAuthority("ROL_ADMIN").antMatchers("/alumnos/addAlumno", "/alumnos/formAlumno/**")
+				.hasAnyAuthority("ROL_ADMIN", "ROL_ALUMNO")
+				.antMatchers("/alumnos/listAlumnos", "/alumnos/deleteAlumno/**", "/alumnos/formAlumno/")
+				.hasAuthority("ROL_ADMIN").antMatchers("/adminPage/**").hasAuthority("ROL_ADMIN").anyRequest()
+				.authenticated())
+				.formLogin((form) -> form.loginPage("/auth/login").defaultSuccessUrl("/home").permitAll())
+				.logout((logout) -> logout.permitAll().logoutUrl("/auth/logout")
+						.logoutSuccessUrl("/auth/login?logout"));
 
 		return http.build();
 	}
-	
+
 	@Bean
-	AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+	AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+			throws Exception {
 		return authenticationConfiguration.getAuthenticationManager();
 	}
 }
